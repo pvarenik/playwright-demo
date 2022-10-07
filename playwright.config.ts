4ed9e7cc794c6bb45b5508a1450d23b0d3e2ab1e
pvarenik <varenikpavel@gmail.com>
2022-10-07 12:30:13 +0200

    allure report publisher

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: [ ['junit', { embedAttachmentsAsProperty: 'testrun_evidence', outputFile: 'results.xml' }] ],
-  reporter: [['line'], ['allure-playwright']],
+  reporter: [['line'], ['allure-playwright', { detail: true, outputFolder: 'allure-results', suiteTitle: true }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
